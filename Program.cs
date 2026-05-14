c304ed8b970cb0661fda111dc8e3f902a941503c
tadaiko <Joanthan.topp.zuniga@cftmail.cl>
2026-05-14 10:56:19 -0400

    Agregar proyecto MiMetadeAhorro

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        string nombre, nombreArticulo;
+        double ahorroDelDia, totalAhorrado = 0;
+
+        Console.Write("Ingrese su nombre: ");
+        nombre = Console.ReadLine();
+
+        Console.Write("Ingrese el nombre del artículo: ");
+        nombreArticulo = Console.ReadLine();
+
+        for (int dia = 1; dia <= 5; dia++)
+        {
+            Console.Write($"Día {dia} - ¿Cuánto ahorraste hoy?: ");
+            ahorroDelDia = double.Parse(Console.ReadLine());
+
+            totalAhorrado += ahorroDelDia;
+        }
+
+        Console.WriteLine("\n----------");
+        Console.WriteLine($"Hola {nombre}, para tu {nombreArticulo}:");
+        Console.WriteLine($"Ahorraste un total de: ${totalAhorrado}");
+
+        if (totalAhorrado >= 15000)
+        {
+            Console.WriteLine("Felicidades! Ya tienes suficiente");
+        }
+        else
+        {
+            double falta = 15000 - totalAhorrado;
+            Console.WriteLine($"Aún no es suficiente, faltan ${falta}");
+            Console.WriteLine("Sigue ahorrando");
+        }
+        Console.WriteLine("----------");
+    }
+}
